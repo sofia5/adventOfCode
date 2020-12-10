f3325dcb36f8e987976ac88c3c0d7984ca601399
Sofia Johansson <sofia5@kth.se>
2020-12-11 00:48:31 +0100

    Done with quiz2 day6

--- 2020/Day6/quiz2.py
+++ 2020/Day6/quiz2.py
@@ -2,12 +2,35 @@ import os
 import sys
 
 
+def getYesResponses(answers):
+    yesAnswers = []
+    numOfPeople = len(answers)
+    allAnswers = ''.join(answers)
+
+    for question in allAnswers:
+        if question not in yesAnswers:
+            if numOfPeople == allAnswers.count(question):
+                yesAnswers.append(question)
+
+    return(len(yesAnswers))
+
+
 def main():
+    allResponses = 0
+    answers = []
 
     with open(os.path.join(sys.path[0], "input.txt")) as f:
         lines = [line.rstrip() for line in f]
 
-    print()
+    for index, line in enumerate(lines):
+        if line != "":
+            answers.append(line)
+
+        if line == "" or index == len(lines) - 1:
+            allResponses += getYesResponses(answers)
+            answers = []
+
+    print(allResponses)
 
 
 if __name__ == "__main__":
