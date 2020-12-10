9b6369ab98595a12dbc2bd633125ab437f36c539
Sofia Johansson <sofia5@kth.se>
2020-12-11 00:21:00 +0100

    quiz1 day 6 done

--- 2020/Day6/quiz1.py
+++ 2020/Day6/quiz1.py
@@ -2,12 +2,30 @@ import os
 import sys
 
 
+def getYesResponses(groupAnswer):
+    yesResonses = []
+    for question in groupAnswer:
+        if question not in yesResonses:
+            yesResonses.append(question)
+    print(yesResonses)
+    return(len(yesResonses))
+
+
 def main():
+    allResponses = 0
+    groupAnswer = ""
 
     with open(os.path.join(sys.path[0], "input.txt")) as f:
         lines = [line.rstrip() for line in f]
 
-    print()
+    for index, line in enumerate(lines):
+        groupAnswer = groupAnswer + line
+
+        if line == "" or index == len(lines) - 1:
+            allResponses += getYesResponses(groupAnswer)
+            groupAnswer = ""
+
+    print(allResponses)
 
 
 if __name__ == "__main__":
