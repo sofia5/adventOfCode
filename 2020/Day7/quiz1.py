e710b3f97fed28c8adb40d07e04e3c2f31b201f4
Sofia Johansson <sofia5@kth.se>
2020-12-11 22:07:49 +0100

    finally done with part1... Failed with the recursive function a few times

--- 2020/Day7/quiz1.py
+++ 2020/Day7/quiz1.py
@@ -2,12 +2,36 @@ import os
 import sys
 
 
-def main():
+def findBags(bags, bagToLookFrom, foundBags):
+    if bagToLookFrom in bags:
+        for bag in bags[bagToLookFrom]:
+            if bag not in foundBags:
+                foundBags.append(bag)
+                findBags(bags, bag, foundBags)
 
+    return foundBags
+
+
+def main():
+    bags = {}
     with open(os.path.join(sys.path[0], "input.txt")) as f:
         lines = [line.rstrip() for line in f]
 
-    print()
+    for line in lines:
+        words = line.split()
+
+        surroundingBag = " ".join(words[: 2])
+
+        for index, word in enumerate(words):
+            if word.isdigit():
+                bag = " ".join(words[index + 1: index + 3])
+                if bag in bags:
+                    bags[bag].append(surroundingBag)
+                else:
+                    bags[bag] = [surroundingBag]
+
+    res = findBags(bags, 'shiny gold', [])
+    print(len(res))
 
 
 if __name__ == "__main__":
