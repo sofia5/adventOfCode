f3e87961e576d7ad3cd7e572d229aa988f8b9916
Sofia Johansson <sofia5@kth.se>
2020-12-12 21:54:29 +0100

    first part of Day9 done

--- 2020/Day9/quiz1.py
+++ 2020/Day9/quiz1.py
@@ -2,12 +2,53 @@ import os
 import sys
 
 
+def getSum(numbers, index1, index2):
+    return numbers[index1] + numbers[index2]
+
+
+def getCorrectSum(numbers, index, sumValue):
+    sumExists = False
+    sumOfNumbers = 0
+    i1 = 0
+
+    while i1 < len(numbers):
+        i2 = 0
+
+        while i2 < len(numbers):
+
+            if i1 != i2:
+                sumOfNumbers = getSum(numbers, i1, i2)
+
+            if sumOfNumbers == sumValue:
+                sumExists = True
+                break
+
+            i2 += 1
+
+        i1 += 1
+
+    return sumExists
+
+
 def main():
+    preamble = 25
+    index = preamble
 
     with open(os.path.join(sys.path[0], "input.txt")) as f:
-        lines = [line.rstrip() for line in f]
+        lines = [int(line.rstrip()) for line in f]
 
-    print()
+    # print(lines)
+    while index < len(lines):
+        sumValue = lines[index]
+        lowerLimit = index - preamble
+        upperLimit = index
+        numbers = lines[lowerLimit:upperLimit].copy()
+        sumExists = getCorrectSum(numbers, index, sumValue)
+        if not sumExists:
+            break
+        index += 1
+
+    print(sumValue)
 
 
 if __name__ == "__main__":
