04d61c0571e5a47a533aba8c756ca739bf149af8
Sofia Johansson <sofia5@kth.se>
2020-12-13 01:50:50 +0100

    hard until the solution was identified. Took some time though...

--- 2020/Day10/quiz1.py
+++ 2020/Day10/quiz1.py
@@ -3,11 +3,30 @@ import sys
 
 
 def main():
+    curPos = 0
+    oneJoltDiffs = 0
+    threeJoltDiffs = 0
 
     with open(os.path.join(sys.path[0], "input.txt")) as f:
-        lines = [line.rstrip() for line in f]
+        lines = [int(line.rstrip()) for line in f]
 
-    print()
+    lines.sort()
+
+    for line in lines:
+
+        if curPos == line - 1:
+            oneJoltDiffs += 1
+
+        elif curPos == line - 3:
+            threeJoltDiffs += 1
+
+        curPos = line
+
+    # Add 3 extra jolts
+    threeJoltDiffs += 1
+
+    print("1-jolt & 3-jolt differences multiplied is: " +
+          str(oneJoltDiffs * threeJoltDiffs))
 
 
 if __name__ == "__main__":
--- 2020/Day10/quiz2.py
+++ 2020/Day10/quiz2.py
@@ -1,13 +1,64 @@
 import os
 import sys
+import math
+
+""" 
+Comments (example):
+1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19
+
+3,1,1,1,3,1,1,3,1,3
+
+- 3,2,1,3,1,1,3,1,3
+- 3,3,3,1,1,3,1,3
+
+4 solutions: 1,1,1 -> 2,1 or 1,2 or 3
+2 solutions: 1,1 -> 2
+"""
+
+
+def getJolts(lines):
+    curPos = 0
+    joltList = ""
+
+    for line in lines:
+        if curPos == line - 1:
+            joltList = joltList + "1"
+
+        elif curPos == line - 3:
+            joltList = joltList + "3"
+
+        curPos = line
+
+    return joltList
+
+
+def getJoltCombinations(joltList):
+    quadruples = 0
+    triples = 0
+    doubles = 0
+    oneJoltGroups = joltList.split("3")
+    for item in oneJoltGroups:
+        if item == '1111':
+            quadruples += 1
+        elif item == '111':
+            triples += 1
+        elif item == '11':
+            doubles += 1
+
+    total = int(math.pow(7, quadruples) *
+                math.pow(4, triples) * math.pow(2, doubles))
+    return total
 
 
 def main():
 
     with open(os.path.join(sys.path[0], "input.txt")) as f:
-        lines = [line.rstrip() for line in f]
+        lines = [int(line.rstrip()) for line in f]
 
-    print()
+    lines.sort()
+    joltList = getJolts(lines)
+    combinations = getJoltCombinations(joltList)
+    print(combinations)
 
 
 if __name__ == "__main__":
