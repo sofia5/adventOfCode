53203c644e589903f19c012eb168849950f8f859
Sofia Johansson <sofia5@kth.se>
2023-12-05 22:44:00 +0100

    fix part2 day 4

--- 2023/day4/part2.ts
+++ 2023/day4/part2.ts
@@ -1,7 +1,14 @@
 import { intersection } from "lodash-es";
 import { readInput } from "../utils.js";
 
-const lines = readInput("day4/testInput.txt");
+const lines = readInput("day4/input.txt");
+
+const cardCopies = Array.from({ length: lines.length }).reduce<
+  Record<number, number>
+>((acc, _, index) => {
+  acc[index] = 1;
+  return acc;
+}, {});
 
 const convertToNumberArray = (numbers: string) =>
   numbers
@@ -9,10 +16,22 @@ const convertToNumberArray = (numbers: string) =>
     .map((num) => Number(num.trim()))
     .filter((item) => item);
 
-for (let i = 0; i <= lines.length; i++) {
-  const numbers = lines[i].substring(lines[i].indexOf(": ") + 1).split("|", 2);
+lines.map((line, index) => {
+  const numbers = line.substring(line.indexOf(": ") + 1).split("|", 2);
   const winningNumbers = convertToNumberArray(numbers[0]);
   const selectedNumbers = convertToNumberArray(numbers[1]);
 
   const numberOfWinning = intersection(winningNumbers, selectedNumbers).length;
-}
+  const curCardCopies = cardCopies[index];
+  Array.from({ length: numberOfWinning }).forEach((_, i) => {
+    const curIndex = index + 1 + i;
+    cardCopies[curIndex] += curCardCopies;
+  });
+});
+
+const sum = Object.values(cardCopies).reduce(
+  (partialSum, a) => partialSum + a,
+  0
+);
+
+console.log(sum);
